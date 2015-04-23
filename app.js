8152e1c6c3daffd690030e367fa36be3cf980706
Victor Miguez <victorcineis@gmail.com>
2015-04-23 18:02:27 -0300

    rotas de views ainda não terminada

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var app = express();
+var express    = require('express');
+var app        = express();
 var bodyParser = require('body-parser');
+var path       = require('path');
 
 // Conexão com o DB
 var bd       = require('./bd');
@@ -14,11 +15,21 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Views e conteúdo estático
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // API
 var api = {}
 api.filmes = require('./api/filmes')
 
 app.use('/api', api.filmes);
+
+// Rotas
+index = require('./routes/index')
+app.use('/', index);
+app.get('/', api.filmes);
 
 // Configurações do servidor
 var port = process.env.PORT || 8080;
